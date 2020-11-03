2d09c011a804c65b23df539caca59ad0042ca721
Hubert Badocha <h.badocha@student.uw.edu.pl>
2020-11-03 10:25:28 +0100

    header + endoding and debug output

--- encstrset.cc
+++ encstrset.cc
@@ -1,6 +1,50 @@
+#include "encstrset.h"
 #include <iostream>
+#include <cstring>
+#include <cassert>
+#ifdef NDEBUG
+constexpr bool DEBUG = false;
+#else
+constexpr bool DEBUG = true;
+#endif
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define print_debug(v) if(DEBUG) std::cerr<<v;
+
+namespace {
+std::string encode(const char *value, const char *key) {
+  assert(value);
+  std::string encoded(value);
+  if (key) {
+    const size_t key_len = strlen(key);
+
+    for (size_t i = 0; i < encoded.size(); i++) {
+      encoded[i] ^= key[i % key_len];
+    }
+  }
+  return encoded;
+}
+}
+
+namespace jnp1 {
+void encstrset_delete(unsigned long id) {
+
+}
+size_t encstrset_size(unsigned long id) {
+  return 0;
+}
+bool encstrset_insert(unsigned long id, const char *value, const char *key) {
+  return false;
+}
+bool encstrset_remove(unsigned long id, const char *value, const char *key) {
+  return false;
+}
+bool encstrset_test(unsigned long id, const char *value, const char *key) {
+  return false;
+}
+void encstrset_clear(unsigned long id) {
+
+}
+void encstrset_copy(unsigned long src_id, unsigned long dst_id) {
+
+}
 }
