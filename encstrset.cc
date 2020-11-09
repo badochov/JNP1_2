5dc05c90539473d634e3b09aa357dbc4bc478f06
Marcin <marcinmike26@gmail.com>
2020-11-09 12:52:51 +0100

    v3 working

--- encstrset.cc
+++ encstrset.cc
@@ -1,7 +1,6 @@
 #include "encstrset.h"
 #include <iostream>
 #include <cstring>
-#include <cassert>
 #include <optional>
 #include <unordered_set>
 #include <unordered_map>
@@ -12,7 +11,7 @@ constexpr bool DEBUG = false;
 constexpr bool DEBUG = true;
 #endif
 
-#define print_debug(v) if(DEBUG) std::cerr<<v
+#define print_debug(v) if(DEBUG) std::cerr << v
 
 namespace {
 
@@ -20,28 +19,27 @@ namespace {
         if (x == nullptr) {
             return "NULL";
         } else {
-            std::cerr << "\"" << x << "\"";
-            return "";
+            return '"' + std::string(x) + '"';
         }
     }
 
     bool is_value_valid(const char* value){
-      return value != nullptr;
+        return value != nullptr;
     }
 
     std::optional<std::string> encode(const char *value, const char *key) {
-      if(!is_value_valid(value)){
-        return {};
-      }
-      std::string encoded(value);
-      if (key) {
-        const size_t key_len = strlen(key);
-
-        for (size_t i = 0; i < encoded.size(); i++) {
-          encoded[i] ^= key[i % key_len];
+        if(!is_value_valid(value)){
+            return {};
         }
-      }
-      return encoded;
+        std::string encoded(value);
+        if (key) {
+            const size_t key_len = strlen(key);
+
+            for (size_t i = 0; i < encoded.size(); i++) {
+                encoded[i] ^= key[i % key_len];
+            }
+        }
+        return encoded;
     }
 
 }
@@ -54,105 +52,140 @@ namespace jnp1 {
     unsigned long encstrset_new() {
         print_debug(__func__ << "()\n");
         std::unordered_set<std::string> new_set;
-        sets.insert({next_id, new_set});
+        sets[next_id] = new_set;
+        print_debug(__func__ << ": set #" << next_id << " created\n");
         ++next_id;
-        print_debug(__func__ << ": set #" << next_id - 1 << " created\n");
         return next_id - 1;
     }
 
     void encstrset_delete(unsigned long id) {
         print_debug(__func__ << "(" << id << ")\n");
+        auto search = sets.find(id);
+        if (search == sets.end()) {
+            print_debug(__func__ << ": set #" << id << " does not exist\n");
+            return;
+        }
         sets.erase(id);
         print_debug(__func__ << ": set #" << id << " deleted\n");
     }
 
     size_t encstrset_size(unsigned long id) {
         print_debug(__func__ << "(" << id << ")\n");
-        try {
-            size_t size = sets.at(id).size();
-            print_debug(__func__ << ": set #" << id << " contains " << size << " element(s)\n");
-            return size;
-        } catch (const std::out_of_range& e) {
+        auto search = sets.find(id);
+        if (search == sets.end()) {
             print_debug(__func__ << ": set #" << id << " does not exist\n");
             return 0;
         }
+        size_t size = search->second.size();
+        print_debug(__func__ << ": set #" << id << " contains " << size
+                    << " element(s)\n");
+        return size;
     }
 
     bool encstrset_insert(unsigned long id, const char* value, const char* key) {
-        print_debug(__func__ << "(" << id << ", " << val(value) << ", " << val(key) << ")\n");
-        std::string encrypted_text = encode(value, key).value_or("NULL");
-        try {
-            bool inserted = sets.at(id).insert(encrypted_text).second;
-            print_debug(__func__ << ": set #" << id << ", cypher \"" << encrypted_text << "\"");
-            if (inserted) {
-                 print_debug(" inserted\n");
-            } else {
-                print_debug(" was already present\n");
-            }
-            return inserted;
-        } catch (const std::out_of_range& e) {
+        print_debug(__func__ << "(" << id << ", " << val(value) << ", "
+                    << val(key) << ")\n");
+        std::optional<std::string> encrypted_text = encode(value, key);
+        if (!encrypted_text.has_value()) {
+            print_debug(__func__ << ": invalid value (NULL)\n");
+            return false;
+        }
+        auto search = sets.find(id);
+        if (search == sets.end()) {
             print_debug(__func__ << ": set #" << id << " does not exist\n");
             return false;
         }
+        bool inserted = search->second.insert(encrypted_text.value()).second;
+        print_debug(__func__ << ": set #" << id << ", cypher \""
+                    << encrypted_text.value() << '"');
+        if (inserted) {
+             print_debug(" inserted\n");
+        } else {
+            print_debug(" was already present\n");
+        }
+        return inserted;
     }
 
     bool encstrset_remove(unsigned long id, const char* value, const char* key) {
-        print_debug(__func__ << "(" << id << ", " << val(value) << ", " << val(key) << ")\n");
-        std::string encrypted_text = encode(value, key).value_or("NULL");
-        try {
-            bool removed = sets.at(id).erase(encrypted_text) > 0;
-            if (removed) {
-                print_debug(__func__ << ": set #" << id << ", cypher \"" << encrypted_text << "\" removed\n");
-            } else {
-                print_debug(__func__ << ": set #" << id << ", cypher \"" << encrypted_text << "\" was not present\n");
-            }
-            return removed;
-        } catch (const std::out_of_range& e) {
+        print_debug(__func__ << "(" << id << ", " << val(value) << ", "
+                    << val(key) << ")\n");
+        std::optional<std::string> encrypted_text = encode(value, key);
+        if (!encrypted_text.has_value()) {
+            print_debug(__func__ << ": invalid value (NULL)\n");
+            return false;
+        }
+        auto search = sets.find(id);
+        if (search == sets.end()) {
             print_debug(__func__ << ": set #" << id << " does not exist\n");
             return false;
         }
+        bool removed = search->second.erase(encrypted_text.value()) > 0;
+        print_debug(__func__ << ": set #" << id << ", cypher \""
+                    << encrypted_text.value() << '"');
+        if (removed) {
+            print_debug(" removed\n");
+        } else {
+            print_debug(" was not present\n");
+        }
+        return removed;
     }
 
     bool encstrset_test(unsigned long id, const char* value, const char* key) {
-        print_debug(__func__ << "(" << id << ", " << val(value) << ", " << val(key) << ")\n");
-        std::string encrypted_text = encode(value, key).value_or("NULL");
-        try {
-            std::unordered_set<std::string> set = sets.at(id);
-            bool present = set.find(encrypted_text) != set.end();
-            if (present) {
-                print_debug(__func__ << ": set #" << id << ", cypher \"" << encrypted_text << "\" is present\n");
-            } else {
-                print_debug(__func__ << ": set #" << id << ", cypher \"" << encrypted_text << "\" is not present\n");
-            }
-            return present;
-        } catch (const std::out_of_range& e) {
+        print_debug(__func__ << "(" << id << ", " << val(value) << ", "
+                    << val(key) << ")\n");
+        std::optional<std::string> encrypted_text = encode(value, key);
+        if (!encrypted_text.has_value()) {
+            print_debug(__func__ << ": invalid value (NULL)\n");
+            return false;
+        }
+        auto search = sets.find(id);
+        if (search == sets.end()) {
             print_debug(__func__ << ": set #" << id << " does not exist\n");
             return false;
         }
+        bool present = search->second.find(encrypted_text.value()) != search->second.end();
+        print_debug(__func__ << ": set #" << id << ", cypher \""
+                    << encrypted_text.value() << '"');
+        if (present) {
+            print_debug(" is present\n");
+        } else {
+            print_debug(" is not present\n");
+        }
+        return present;
     }
 
     void encstrset_clear(unsigned long id) {
         print_debug(__func__ << "(" << id << ")\n");
-        try {
-            sets.at(id).clear();
-            print_debug(__func__ << ": set #" << id << " cleared\n");
-        } catch (const std::out_of_range& e) {
+        auto search = sets.find(id);
+        if (search == sets.end()) {
             print_debug(__func__ << ": set #" << id << " does not exist\n");
+            return;
         }
+        search->second.clear();
+        print_debug(__func__ << ": set #" << id << " cleared\n");
     }
 
     void encstrset_copy(unsigned long src_id, unsigned long dst_id) {
         print_debug(__func__ << "(" << src_id << ", " << dst_id << ")\n");
-        auto src_set = sets.find(src_id);
-        auto dst_set = sets.find(dst_id);
-        if (src_set != sets.end() && dst_set != sets.end()) {
-            for (const std::string& s : (*src_set).second) {
-                bool inserted = (*dst_set).second.insert(s).second;
-                if (inserted) {
-                    print_debug(__func__ << ": cypher \"" << s << "\" copied from set #" << src_id << " to set #" << dst_id << "\n");
-                } else {
-                    print_debug(__func__ << ": copied cypher \"" << s << "\" was already present in set #" << dst_id << "\n");
-                }
+        auto src_search = sets.find(src_id);
+        auto dst_search = sets.find(dst_id);
+        if (src_search == sets.end()) {
+            print_debug(__func__ << ": set #" << src_id << " does not exist\n");
+            return;
+        }
+        if (dst_search == sets.end()) {
+            print_debug(__func__ << ": set #" << dst_id << " does not exist\n");
+            return;
+        }
+        for (const std::string& s : src_search->second) {
+            bool inserted = dst_search->second.insert(s).second;
+            if (inserted) {
+                print_debug(__func__ << ": cypher \"" << s
+                            << "\" copied from set #" << src_id
+                            << " to set #" << dst_id << "\n");
+            } else {
+                print_debug(__func__ << ": copied cypher \"" << s
+                            << "\" was already present in set #" << dst_id << "\n");
             }
         }
     }
